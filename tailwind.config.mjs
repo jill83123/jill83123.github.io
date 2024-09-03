b553a64f899a9bbb761439c1ea2997c2704f0b17
YU-FANG <jill83123@gmail.com>
2024-09-03 23:50:03 +0800

    ✨ feat: 初步設定基本版型與全域樣式

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,25 @@ const { addDynamicIconSelectors } = require('@iconify/tailwind');
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '12px',
+    },
+    fontFamily: {
+      sans: ['Inter', 'Noto Sans TC', ...defaultTheme.fontFamily.sans],
+      mono: ['Roboto Mono', ...defaultTheme.fontFamily.mono],
+    },
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        deco: {
+          light: 'var(--color-deco-light)',
+          dark: 'var(--color-deco-dark)',
+        },
+      },
+    },
   },
   plugins: [addDynamicIconSelectors()],
 };
